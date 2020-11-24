067d32fb356fd29444b004260e7d1bf37abfad2f
AnhMH <anh.mh@moziasoft.com>
2020-11-24 12:21:13 +0700

    Add button view more

--- resources/views/layout/image.blade.php
+++ resources/views/layout/image.blade.php
@@ -1,5 +1,9 @@
+<?php
+$page = !empty($params['page']) ? $params['page'] + 1 : 2;
+?>
 <div class="col">
     <div class="card box-shadow">
+        <a href="{{ url('/images?page=').$page }}" class="btn btn-danger btn-image">View more</a>
         <img class="g-image" src="{{ $img->url }}" alt="SBGC image {{ $img->id }}">
         <div class="fb-like" data-href="{{ route('home.imageDetail', ['id' => $img->id]) }}" data-width="" data-layout="button" data-action="like" data-size="small" data-share="true"></div>
     </div>
