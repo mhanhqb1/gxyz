4cd865f9f7167431624a6935af99e90455c940f5
AnhMH <anh.mh@moziasoft.com>
2020-11-24 12:10:13 +0700

    Change button like share

--- resources/views/layout/image.blade.php
+++ resources/views/layout/image.blade.php
@@ -1,6 +1,6 @@
 <div class="col">
     <div class="card box-shadow">
         <img class="g-image" src="{{ $img->url }}" alt="SBGC image {{ $img->id }}">
-        <div class="fb-like" data-href="{{ route('home.imageDetail', ['id' => $img->id]) }}" data-width="" data-layout="standard" data-action="like" data-size="small" data-share="true"></div>
+        <div class="fb-like" data-href="{{ route('home.imageDetail', ['id' => $img->id]) }}" data-width="" data-layout="button" data-action="like" data-size="small" data-share="true"></div>
     </div>
 </div>
