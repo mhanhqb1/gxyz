3a4c7c09f70a55cf78e19ee2eb3177e35c28e016
AnhMH <anh.mh@moziasoft.com>
2020-11-29 14:29:50 +0700

    Add new ads

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -132,13 +132,6 @@ $_siteDescription = 'Sexy Girl, Hot Girl, Cute Girl, Beautiful Girl';
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <?php if (!empty(config('app.ads_yllix')['pub_id'])): ?>
-        <?php if (!empty(rand(0,1))): ?>
-        <script type="text/javascript" src="https://buleor.com/tun.php?section=General&pt=8&pub={{ config('app.ads_yllix')['pub_id'] }}&ga=a"></script>
-        <?php else: ?>
-        <script type="text/javascript" src="https://buleor.com/pun.php?section=General&pt=6&pub={{ config('app.ads_yllix')['pub_id'] }}&ga=a"></script>
-        <?php endif; ?>
-        <!--<script src="https://cdn-server.top/p/wl.js?pub={{ config('app.ads_yllix')['pub_id'] }}&ga=a"></script>-->
-    <?php endif; ?>
+    @include('layout.ads_footer')
 </body>
 </html>
