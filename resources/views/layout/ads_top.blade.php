b40003d51f6f1647abc4929e8fe5ad3169833b8c
AnhMH <anh.mh@moziasoft.com>
2020-11-30 15:14:38 +0700

    Add new ads

--- resources/views/layout/ads_top.blade.php
+++ resources/views/layout/ads_top.blade.php
@@ -8,3 +8,5 @@
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.displaycontentprofit.com/8be6b932bcd4165e7e86133464c5876f/invoke.js"></scr' + 'ipt>');
 </script>
+
+<script src="https://static.adxadserv.com/js/adb.js" type="text/javascript" data-adxad-place="5fc49c3961d6e2490d181e52" async></script><div id="5fc49c3961d6e2490d181e52" data-width="300" data-height="250" data-output="html"></div>
