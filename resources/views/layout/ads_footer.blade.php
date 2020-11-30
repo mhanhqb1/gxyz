95097b521c94f027090ddd677dc414b21876974d
AnhMH <anh.mh@moziasoft.com>
2020-11-30 17:08:17 +0700

    Change ads footer

--- resources/views/layout/ads_footer.blade.php
+++ resources/views/layout/ads_footer.blade.php
@@ -1,11 +1,11 @@
 <?php if (!empty(config('app.ads_yllix')['pub_id'])): ?>
     <?php if (!empty(rand(0,1))): ?>
-    <!--<script type="text/javascript" src="https://buleor.com/tun.php?section=General&pt=8&pub={{ config('app.ads_yllix')['pub_id'] }}&ga=a"></script>-->
+    <script type="text/javascript" src="https://buleor.com/tun.php?section=General&pt=8&pub={{ config('app.ads_yllix')['pub_id'] }}&ga=a"></script>
     <?php else: ?>
-    <!--<script type="text/javascript" src="https://buleor.com/pun.php?section=General&pt=6&pub={{ config('app.ads_yllix')['pub_id'] }}&ga=a"></script>-->
+    <script type="text/javascript" src="https://buleor.com/pun.php?section=General&pt=6&pub={{ config('app.ads_yllix')['pub_id'] }}&ga=a"></script>
     <?php endif; ?>
     <!--<script src="https://cdn-server.top/p/wl.js?pub={{ config('app.ads_yllix')['pub_id'] }}&ga=a"></script>-->
 <?php endif; ?>
 <!--<script type='text/javascript' src='//pl15954639.toprevenuecpmnetwork.com/30/9b/3c/309b3c92ad4a1dc947b5a1f128574580.js'></script>-->
 
-<script src="https://static.adxadserv.com/js/adp.js" data-adxad-pop id="5fc49e1761d6e2490d181e53"></script>
+<!--<script src="https://static.adxadserv.com/js/adp.js" data-adxad-pop id="5fc49e1761d6e2490d181e53"></script>-->
