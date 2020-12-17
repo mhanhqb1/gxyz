50189fa7e18712c6abe10c1ff093e66cf857e6f3
AnhMH <anh.mh@moziasoft.com>
2020-12-17 17:58:02 +0700

    Modify yillix ads

--- resources/views/layout/ads_top.blade.php
+++ resources/views/layout/ads_top.blade.php
@@ -1,4 +1,8 @@
 <?php if (!empty(config('app.ads_yllix')['pub_id'])): ?>
-    <script type="text/javascript" src="https://sailif.com/bnr.php?section=General&pub={{ config('app.ads_yllix')['pub_id'] }}&format={{ config('app.ads_yllix')['format'] }}&ga=a"></script>
-    <div style="display: block; margin: 10px auto; text-align: center;"><noscript><a href="https://yllix.com/publishers/{{ config('app.ads_yllix')['pub_id'] }}" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub/{{ config('app.ads_yllix')['format'] }}.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /></a></noscript></div>
+    <script type="text/javascript" src="https://sailif.com/bnr.php?section=General&pub={{ config('app.ads_yllix')['pub_id'] }}&format={{ config('app.ads_yllix')['format'] }}&ga=a&bg=1"></script>
+    <noscript>
+        <a href="https://yllix.com/publishers/{{ config('app.ads_yllix')['pub_id'] }}" target="_blank">
+            <img src="//ylx-aff.advertica-cdn.com/pub/{{ config('app.ads_yllix')['format'] }}" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" />
+        </a>
+    </noscript>
 <?php endif; ?>
