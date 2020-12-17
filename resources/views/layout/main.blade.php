20ff50e3a600c225189c73c2b1f66cde2e8a6418
AnhMH <anh.mh@moziasoft.com>
2020-12-17 21:06:33 +0700

    Add new ads

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10,7 +10,7 @@ $_currentUrl = url()->current();
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta name="clckd" content="cce8e6015f45ff1763849c7c4689b6d1" />
-        <meta name="exoclick-site-verification" content="3cc6e02ee10ef312b550e7dfde386c91">
+        <script data-cfasync="false" type="text/javascript" src="//nicksstevmark.com/bultykh/ipp24/7/bazinga/1805702" async></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="{{ $_siteDescription }}">
