5094738ddb5b6d6cdbbf1827090bf61a296922ab
AnhMH <anh.mh@moziasoft.com>
2020-12-17 16:59:28 +0700

    Add clickadu

--- resources/views/layout/ads_footer.blade.php
+++ resources/views/layout/ads_footer.blade.php
@@ -6,6 +6,7 @@
     <?php endif; ?>
     <!--<script src="https://cdn-server.top/p/wl.js?pub={{ config('app.ads_yllix')['pub_id'] }}&ga=a"></script>-->
 <?php endif; ?>
+<script data-cfasync="false" type="text/javascript" src="//runbornto.com/t/9/fret/meow4/1805398/brt.js"></script>
 <!--<script type='text/javascript' src='//pl15954639.toprevenuecpmnetwork.com/30/9b/3c/309b3c92ad4a1dc947b5a1f128574580.js'></script>-->
 
 <!--<script src="https://static.adxadserv.com/js/adp.js" data-adxad-pop id="5fc49e1761d6e2490d181e53"></script>-->
