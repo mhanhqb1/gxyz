1faea9ed8a02d1ef8e6742474684ec3c5c206fbd
AnhMH <anh.mh@moziasoft.com>
2020-12-20 23:24:07 +0700

    Add instance ads

--- resources/views/layout/ads_footer.blade.php
+++ resources/views/layout/ads_footer.blade.php
@@ -1,5 +1,5 @@
-
-<script data-cfasync="false" type="text/javascript" src="//runbornto.com/t/9/fret/meow4/1805398/brt.js"></script>
+<script data-cfasync="false" async="async" type="text/javascript" src="//sevenedgesteve.com/q/tdl/95/dnt/1805665/kep.js"></script>
+<!--<script data-cfasync="false" type="text/javascript" src="//runbornto.com/t/9/fret/meow4/1805398/brt.js"></script>-->
 <!--<script type='text/javascript' src='//pl15954639.toprevenuecpmnetwork.com/30/9b/3c/309b3c92ad4a1dc947b5a1f128574580.js'></script>-->
 
 <!--<script src="https://static.adxadserv.com/js/adp.js" data-adxad-pop id="5fc49e1761d6e2490d181e53"></script>-->
