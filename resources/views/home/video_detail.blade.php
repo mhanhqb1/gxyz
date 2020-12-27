0feb293a67f091b9c16d7606a87884d117027417
AnhMH <anh.mh@moziasoft.com>
2020-12-27 14:08:06 +0700

    Modify ads footer

--- resources/views/home/video_detail.blade.php
+++ resources/views/home/video_detail.blade.php
@@ -34,7 +34,4 @@
     </div>
 
 </div><!-- /.row -->
-<?php if (!empty(config('app.ads_yllix')['pub_id'])): ?>
-<!--<script type="text/javascript" src="https://buleor.com/mobile_redir.php?section=General&pub={{ config('app.ads_yllix')['pub_id'] }}&ga=a"></script>-->
-<?php endif; ?>
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -128,21 +128,6 @@ $_currentUrl = url()->current();
                         <img src="{{ asset('imgs/thanks.gif') }}"/>
                     </div>-->
     </footer>
-    
-    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" id="adsModal">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <iframe src="https://rtyznd.com/i/bfdsrtc/cmsp/1805667/kd" width="100%" height="500px"></iframe>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
@@ -150,9 +135,10 @@ $_currentUrl = url()->current();
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
     <script>
         $(document).ready(function(){
-            $('#adsModal').modal();
+           
         });
     </script>
     @include('layout.ads_footer')
