72b0b542cb36f6a7d7ad7f7b78e1f1807b4284ba
AnhMH <anh.mh@moziasoft.com>
2021-01-08 15:22:40 +0700

    Modify ads footer

--- resources/views/layout/ads_footer.blade.php
+++ resources/views/layout/ads_footer.blade.php
@@ -1,6 +1,5 @@
-<script data-cfasync="false" type="text/javascript" src="//blastcahs.com/t/9/fret/meow4/1809584/brt.js"></script>
 <script data-cfasync="false" async="async" type="text/javascript" src="//sevenedgesteve.com/q/tdl/95/dnt/1809599/kep.js"></script>
-<script data-cfasync="false" type="text/javascript" src="//nicksstevmark.com/bultykh/ipp24/7/bazinga/1809598" async></script>
+<script data-cfasync="false" type="text/javascript" src="//blastcahs.com/t/9/fret/meow4/1809584/brt.js"></script>
 <!--<script type='text/javascript' src='//pl15954639.toprevenuecpmnetwork.com/30/9b/3c/309b3c92ad4a1dc947b5a1f128574580.js'></script>-->
 
 <!--<script src="https://static.adxadserv.com/js/adp.js" data-adxad-pop id="5fc49e1761d6e2490d181e53"></script>-->
