5d9983d7e7cc120f6c699aa15ea34e3891b082bc
AnhMH <anh.mh@moziasoft.com>
2021-08-12 22:24:59 +0700

    Fix ads

--- resources/views/layout/ads_footer.blade.php
+++ resources/views/layout/ads_footer.blade.php
@@ -3,4 +3,4 @@
 <!--<script type='text/javascript' src='//pl15954639.toprevenuecpmnetwork.com/30/9b/3c/309b3c92ad4a1dc947b5a1f128574580.js'></script>-->
 
 <!--<script src="https://static.adxadserv.com/js/adp.js" data-adxad-pop id="5fc49e1761d6e2490d181e53"></script>-->
-<script type="text/javascript" src="https://buleor.com/pun.php?section=General&pt=6&pub=771288&ga=a&fN=1&fT=2&fTT=1"></script>
+<script type="text/javascript" src="https://buleor.com/pun.php?section=General&pt=6&pub=771288&ga=a&fN=2&fT=5&fTT=1"></script>
