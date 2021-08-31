5c7d6c80533704ee5f83709a7bc726cae7d05f7a
AnhMH <anh.mh@moziasoft.com>
2021-08-31 18:42:07 +0700

    Fix home page

--- resources/views/home/new_index.blade.php
+++ resources/views/home/new_index.blade.php
@@ -21,6 +21,17 @@
 </section>
 
 <div class="section-title">
+    <h2>Hot Videos</h2>
+    <a href="{{ route('home.videos') }}" title="View more">View more</a>
+</div>
+<section class="lastest-posts">
+    <?php foreach ($videos as $k => $v) : ?>
+        <?php if ($k <= 3) { continue; } ?>
+        @include('layout.card_item', ['item' => $v])
+    <?php endforeach; ?>
+</section>
+
+<div class="section-title">
     <h2>Sexy Videos</h2>
     <a href="{{ route('home.18videos') }}" title="View more">View more</a>
 </div>
